bca49d3ff41d7e5e328de1d2bd2bf83bf7fbf3ca
oliviertech212 <oliviertech212@gmail.com>
2024-03-11 08:57:31 +0200

    handle grpaphql playground

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,37 @@
-import { ApolloServer } from "@apollo/server";
-import { PrismaClient, User, Message } from "@prisma/client";
-import { startStandaloneServer } from "@apollo/server/standalone";
-import { builder } from "./builder";
+import { ApolloServer } from "apollo-server";
+import { ApolloServerPluginLandingPageLocalDefault } from "apollo-server-core";
+import { ApolloServerPluginLandingPageProductionDefault } from "apollo-server-core";
+import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
+import { PrismaClient } from "@prisma/client";
+
 import { schema } from "./schema";
 const prisma = new PrismaClient();
 
-const server = new ApolloServer({ schema });
+export const server = new ApolloServer({
+  schema,
+  context: async ({ req, res }) => ({
+    req,
+  }),
+  plugins: [
+    process.env.NODE_ENV === "production"
+      ? ApolloServerPluginLandingPageProductionDefault({
+          graphRef: "my-graph-id@my-graph-variant",
+          footer: false,
+        })
+      : ApolloServerPluginLandingPageLocalDefault({ footer: false }),
+  ],
+});
+
+const port = process.env.PORT || 4000;
 
 const app = async () => {
   try {
     await prisma.$connect();
     console.log("Connected to the database");
-    const url = await startStandaloneServer(server, {
-      listen: { port: process.env.PORT as any },
-      context: async ({ req, res }) => ({
-        req,
-      }),
+
+    server.listen({ port }).then(({ url }) => {
+      console.log(`  Server ready at ${url}`);
     });
-    console.log(
-      `🚀  Server ready at:    http://localhost:${process.env.PORT}     `
-    );
   } catch (error) {
     console.error(error);
   }
